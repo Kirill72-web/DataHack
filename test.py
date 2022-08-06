8f72410e5a7fd4fc2f9a2bdba1c4f6b33f699714
sokil26 <sokil26@yandex.ru>
2022-08-06 17:01:08 +0300

    Test commit 2

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import subprocess
 import time
 import os
+import pandas as pd
+import numpy as np
+
 
 start = time.time()
 process = subprocess.run(['python', 'generator.py', '-f', 'example', '-j', 'commands.json', '-r', '10000'], capture_output=True)
 delta = time.time() - start
-if ' '.join(os.listdir()).find('example.csv') != -1:
-    print('Test 1: Done, time:', delta)
-else:
-    print('Test 1: Failed:\n', process.stderr if process.stderr else 'file not generated')
 
+if process.stderr:
+    print('Test 1: Failed:\n', process.stderr)
+    exit()
+elif ' '.join(os.listdir()).find('example.csv') == -1:
+    print('Test 1: Failed:\n', 'file not generated')
+    exit()
+print('Test 1: Done, time:', delta)
 
 
