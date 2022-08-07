7d2a0354446a24ac38516cc9f229f357b3f68d41
sokil26 <sokil26@yandex.ru>
2022-08-07 08:25:50 +0300

    test commit Alias test

--- example_join_s.py
+++ example_join_s.py
@@ -5,11 +5,8 @@ class Table:
     string = String((['a', 'b', 'c'], 2))
     choice = SetChoice(['123', '456', 'hello'])
     mask = Mask(["123##456#", True, True])
-    float = Float((1, 10))
+    key = Alias("key")
     integer = Integer((1, 10))
     wchoice = WeighedChoice((['123', '456', 'hello'], [10, 20, 70]))
     date = Date(("2022-10-07", "2022-10-12"))
     timestep = TimeStep(("2022-10-07 19:45:30", "2022-10-12 19:45:30"))
-
-
-
